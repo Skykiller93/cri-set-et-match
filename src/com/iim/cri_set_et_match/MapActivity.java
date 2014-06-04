ab0fb9e8bbde3d7b1a93e6f4405029b9ed9e16aa
Adrien Kühn <adrien.kuhn@gmx.com>
2014-06-04 16:47:30 +0200

    Fix.

--- src/com/iim/cri_set_et_match/MapActivity.java
+++ src/com/iim/cri_set_et_match/MapActivity.java
@@ -15,7 +15,6 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 
 public class MapActivity extends Activity implements LocationListener{
