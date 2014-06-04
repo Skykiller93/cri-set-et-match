c39a48dfd236eb3e8fe6313a3eb55eca0b2cb97a
Adrien Kühn <adrien.kuhn@gmx.com>
2014-06-04 17:48:40 +0200

    Jeu et Map fonctionnelle.

--- src/com/iim/cri_set_et_match/MapActivity.java
+++ src/com/iim/cri_set_et_match/MapActivity.java
@@ -15,17 +15,18 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 
 public class MapActivity extends Activity implements LocationListener{
-	
+
 	private GoogleMap maps;
 	private MapFragment mapFragment;
 	private String string = null;
-	
+
 	private String[] data;
 	private String[] geo;
-	
+
 	private LocationManager locationManager;
 
 	@Override
@@ -35,20 +36,20 @@ public class MapActivity extends Activity implements LocationListener{
 
 		mapFragment = (MapFragment) this.getFragmentManager().findFragmentById(R.id.map);
 		maps = mapFragment.getMap();
-		
+
 		// modification settings map
-		
+
 		UiSettings settings = maps.getUiSettings();
 		settings.setZoomControlsEnabled(true);
 		settings.setRotateGesturesEnabled(true);
 		settings.setScrollGesturesEnabled(true);
 		settings.setTiltGesturesEnabled(true);
-		
+
 		// Marqueur
 		MarkerOptions options = new MarkerOptions();
 		Calendar mc = Calendar.getInstance();
 		int day = mc.get(Calendar.DAY_OF_YEAR);
-		
+
 		if(day<120){
 			//Australia
 			string = getResources().getString(R.string.mp);
@@ -65,33 +66,33 @@ public class MapActivity extends Activity implements LocationListener{
 			// Else
 			string = getResources().getString(R.string.rg);
 		}
-		
+
 		data = string.split(";");
 		geo = data[2].split(",");
-		
+
 		LatLng court = new LatLng(Float.parseFloat(geo[0]), Float.parseFloat(geo[1]));
 		options.position(court);
-		
+
 		options.title(data[0]);
 		options.snippet(data[1]);
 		options.draggable(true);
 		Marker lieu = maps.addMarker(options);
-		
+
 		//My Location
 		locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-		if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+		if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)){
 			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
 			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
 		}
-		
+
 		// Zoom sur le point
 		maps.moveCamera(CameraUpdateFactory.newLatLngZoom(lieu.getPosition(),17));
-		
+
 		// changement type carte
 		maps.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-		
+
 		maps.animateCamera(CameraUpdateFactory.newLatLngZoom(lieu.getPosition(),17), 2000, null);
-		
+
 	}
 
 	@Override
@@ -118,24 +119,19 @@ public class MapActivity extends Activity implements LocationListener{
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Stub de la méthode généré automatiquement
-		
+
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
 		// TODO Stub de la méthode généré automatiquement
-		
+
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
 		// TODO Stub de la méthode généré automatiquement
-		
-	}
-	
-	@Override
-	public void onStop(){
-		
+
 	}
 
 }
