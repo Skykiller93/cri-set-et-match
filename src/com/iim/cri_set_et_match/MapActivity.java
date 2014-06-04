b521b03846194b325dd4d9e546ba73c4c6c46d92
Adrien Kühn <adrien.kuhn@gmx.com>
2014-06-04 16:18:03 +0200

    Affichage de la position de l'user si distance < 20km

--- src/com/iim/cri_set_et_match/MapActivity.java
+++ src/com/iim/cri_set_et_match/MapActivity.java
@@ -11,15 +11,23 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 
-public class MapActivity extends Activity {
+public class MapActivity extends Activity implements LocationListener{
 	
 	private GoogleMap maps;
 	private MapFragment mapFragment;
 	private String string = null;
+	
+	private String[] data;
+	private String[] geo;
+	
+	private LocationManager locationManager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +49,12 @@ public class MapActivity extends Activity {
 		MarkerOptions options = new MarkerOptions();
 		Calendar mc = Calendar.getInstance();
 		int day = mc.get(Calendar.DAY_OF_YEAR);
-
+		
 		if(day<120){
 			//Australia
 			string = getResources().getString(R.string.mp);
 		}else if(day < 160){
-			// Rolang Garos
+			// Roland Garros
 			string = getResources().getString(R.string.rg);
 		}else if(day < 200){
 			//Wimbledon
@@ -59,22 +67,29 @@ public class MapActivity extends Activity {
 			string = getResources().getString(R.string.rg);
 		}
 		
-		String[] data = string.split(";");
-		String[] geo = data[2].split(",");
-		options.position(new LatLng(Float.parseFloat(geo[0]), Float.parseFloat(geo[1])));
+		data = string.split(";");
+		geo = data[2].split(",");
+		
+		LatLng court = new LatLng(Float.parseFloat(geo[0]), Float.parseFloat(geo[1]));
+		options.position(court);
+		
 		options.title(data[0]);
 		options.snippet(data[1]);
+		options.draggable(true);
 		Marker lieu = maps.addMarker(options);
 		
-		options.draggable(true);
+		//My Location
+		locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+		if(locationManager.isProviderEnabled(locationManager.GPS_PROVIDER)){
+			locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 0, this);
+			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 10000, 0, this);
+		}
 		
 		// Zoom sur le point
 		maps.moveCamera(CameraUpdateFactory.newLatLngZoom(lieu.getPosition(),17));
 		
 		// changement type carte
 		maps.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-		// Localisation de l'user
-		maps.setMyLocationEnabled(true);
 		
 		maps.animateCamera(CameraUpdateFactory.newLatLngZoom(lieu.getPosition(),17), 2000, null);
 		
@@ -86,6 +101,37 @@ public class MapActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Stub de la méthode généré automatiquement
+		// Distance
+        Location destination = new Location("Destination");
+        destination.setLatitude(Float.parseFloat(geo[0]));
+        destination.setLongitude(Float.parseFloat(geo[1]));
+        
+		if(location.distanceTo(destination) < 20000){
+        	maps.setMyLocationEnabled(true);
+        }
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Stub de la méthode généré automatiquement
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Stub de la méthode généré automatiquement
+		
 	}
 
 }
